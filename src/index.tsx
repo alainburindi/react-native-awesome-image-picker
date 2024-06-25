66224af38ebf50a3b40ac24af11b94ef86ec4e1c
alainburindi <alainburindi62@gmail.com>
2024-06-25 17:51:45 +0200

    chore: replace the test app

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
+import { Image, Text, View } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export function ImagePicker() {
+  return (
+    <View>
+      <Text>Image Picker</Text>
+      <Image source={require('./assets/images/close.jpg')} />
+    </View>
+  );
 }
